7e0ab6c4e647cd3da05bc1c30f140d20629c3278
GustyCube <gc@gustycube.xyz>
2024-10-20 17:36:07 -0400

    Co-authored-by: Joshua <Thatoddone1@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 setup(
     name='simplecli',
     version='0.1.0',
-    packages=find_packages(),  # Automatically find packages
+    packages=find_packages(),  
     entry_points={
         'console_scripts': [
-            'simplecli = simplecli_pkg.__main__:main'  # Entry point for the CLI
+            'simplecli = simplecli_pkg.__main__:main' 
         ]
     }
 )
--- simplecli_pkg/funcmodule.py
+++ simplecli_pkg/funcmodule.py
@@ -1,2 +1,14 @@
+import ollama
+
 def my_function(text_to_display):
     print('text from my_function :: {}'.format(text_to_display))
+
+
+def ollama_module(text):
+   ollama.chat(model=modelName, messages=[
+  {
+    'role': 'user',
+    'content': text,
+    
+  },
+]) 
